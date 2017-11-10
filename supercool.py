0c34cd72031620794f142128118188910262adef
Bailey <abailey@20airabailey.shipleyschool.net>
2017-11-10 12:58:59 -0500

    Some minor bugs, also began third phase: 'Select'

--- supercool.py
+++ supercool.py
@@ -16,7 +16,7 @@ verbList = glossary("aiden_verbs.txt")
 prepList = glossary("preps.txt")
 advList = glossary("adverbs.txt")
 
-term = raw_input("Enter a term: ") # You might need to change this to input() for your python version
+ex_term = raw_input("Enter a term: ") # You might need to change this to input() for your python version
 
 preps = []
 for prep in prepList:
@@ -46,7 +46,7 @@ def conj(verb): #takes [paro,parare,paravi,paratus,prepare,1] returns [1,par,par
         else: conj = "3"
         if dep: conj += "D"
         elif semidep: conj += "S"
-        if "-" in verb: conj = "def"
+        elif "-" in verb: conj = "def"
 
         primary = verb[0][:-1]
         if dep: primary = verb[0][:-2]
@@ -84,28 +84,27 @@ for verb in verbList:
 #if you run this, you might get an error here
 #that's because of some irregular verbs in the  list
 
-#Phase: Identify
-for term in [term]:
-    pos = []
+#Phase: Assemble
+pos = []
+for term in [ex_term]:
+    pos.append([term])
 
     adv = inc(term,advs) #in reality adv = (is term an adverb?)
-    if adv[0]: pos.append(adv[1])
+    if adv[0]: pos[-1].append(["adv",adv[1]])
     prep = inc(term,preps)
-    if prep[0]: pos.append(prep[1])
+    if prep[0]: pos[-1].append(["prep",prep[1]])
 
     for stem in vstems:
         #print "Fake:",stem
         if inc(term[0],stem[1:3]):
-            fake = True
-            if stem[1] == term[:len(stem[1])]: fake = ["p",stem]
-            if stem[2][:-1] == term[:len(stem[2])-1]: fake = ["pf",stem]
-            if fake != True: pos.append(fake)
+            if stem[1] == term[:len(stem[1])]: pos[-1].append(["v","p",stem])
+            if stem[2][:-1] == term[:len(stem[2])-1]: pos[-1].append(["v","pf",stem])
 
-    prns = [["hic","this",5],["qui","-",6],["is","weak dem",3]]
-    pres = [["h"],["qu","cui"],["i","e"]]
+    prns = [["hic","this",5],["qui","-",6],["is","weak dem",3]] #id, def, max length
+    pres = [["h"],["qu","cui"],["i","e"]] #prefixes (word-beginnings)
     for i in range(len(pres)):
         for pre in pres[i]:
-            if len(term) <= prns[i][2] and term[:len(pre)] == pre: pos.append([prns[i][:2]])
+            if len(term) <= prns[i][2] and term[:len(pre)] == pre: pos[-1].append(["prn",prns[i][:2]])
 
     #nouns
 
@@ -113,4 +112,43 @@ for term in [term]:
 
     print pos
 
-#for p in pos:
+#Phase: Select
+final_list = []
+conflict
+personal = ["o","s","t","mus","tis","nt","or","ris","tur","mur","mini","ntur"]
+persons = ["1S","2S","3S","1P","2P","3P"]
+for pts in pos: #possible terms
+    fins = [] #finalists
+    term = pts.pop(0)
+    for p in pts: #possibility
+        if p[0] in ["adv","prep"]: fins.append(p)
+        elif p[0] == "v":
+            #Intensive verb identifier, all tenses
+            forms = []
+            stem = p[2]
+            if p[1] == "p":
+                pers = personal
+                if stem[0] == "1": pers[1:].insert(0,"a")
+                elif stem[0] == "3": pers[1:-1].insert(0,"i")
+                if  stem[0] in ["3","3i","4"]: pers[-1].insert(0,"u")
+                for end_id in range(len(pers)):
+                    if end_id < 6: voice = "A"
+                    else: voice = "P"
+                    forms.append([stem[1]+pers[end_id],"%s-P-I-%s" % (persons[end_id],voice])
+
+                pers = personal
+                pers[0] = "m"
+                for end_id in range(len(pers)):
+                    if end_id < 6: voice = "A"
+                    else: voice = "P"
+                    forms.append([stem[3]+"ba"+pers[end_id],"%s-Impf-I-%s" % (persons[end_id],voice])
+                #not done yet
+            else:
+                #perfect
+        elif p[0] == "prn":
+            stem = p[1]
+        elif p[0] == "n":
+
+        elif p[0] == "adj":
+
+    if len(fins) != 1: fins.insert(0,"conf") #conflict
