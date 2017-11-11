597dd671207830f4ddbad383fa88413635f9c086
Bailey <abailey@20airabailey.shipleyschool.net>
2017-11-11 09:58:31 -0500

    Infinitives, some of nouns

--- supercool.py
+++ supercool.py
@@ -1,6 +1,6 @@
 def inc(term,list): # efficiently returns (term in list) boolean
     for item in list:
-        if term == item[0]: # [0] important
+        if term == item[0]: #[0] important
             return [True,item]
     return [False]
 
@@ -15,6 +15,7 @@ def glossary(url):
 verbList = glossary("aiden_verbs.txt")
 prepList = glossary("preps.txt")
 advList = glossary("adverbs.txt")
+nounList = [] #glossary("nouns.txt")
 
 ex_term = raw_input("Enter a term: ") # You might need to change this to input() for your python version
 
@@ -82,6 +83,19 @@ for verb in verbList:
 #if you run this, you might get an error here
 #that's because of some irregular verbs in the  list
 
+decls = {"ae":1,"i":2,"is":3,"us":4,"ei":5}
+for i in range(10):
+    print decls
+def decl(noun):
+    [nom,gen,gend] = noun
+    for s in decls:
+        if s == gen[-len(s):]: suf = s
+    return [decls[suf],nom,gen[:-len(suf)],gend.upper()]
+
+nstems = []
+for noun in nounList:
+    nstems.append(decl(noun))
+
 #Phase: Assemble
 pos = []
 for term in [ex_term]:
@@ -94,9 +108,10 @@ for term in [ex_term]:
 
     for stem in vstems:
         #print "Fake:",stem
-        if inc(term[0],stem[1:3]):
-            if stem[1] == term[:len(stem[1])]: pos[-1].append(["v","p",stem])
-            if stem[2] != "-" and stem[2][:-1] == term[:len(stem[2])-1]: pos[-1].append(["v","pf",stem])
+        if stem[1] == term[:len(stem[1])]: pos[-1].append(["v","p",stem])
+        if stem[2] != "-" and stem[2][:-1] == term[:len(stem[2])-1]: pos[-1].append(["v","pf",stem])
+        if stem[3] == term[:len(stem[3])]: pos[-1].append(["v","ppl",stem])
+        if stem[3] == term[:len(stem[3])]: pos[-1].append(["v","ppp",stem])
 
     prns = [["hic","this",5],["qui","-",6],["is","weak dem",3]] #id, def, max length
     pres = [["h"],["qu","cui"],["i","e"]] #prefixes (word-beginnings)
@@ -105,6 +120,8 @@ for term in [ex_term]:
             if len(term) <= prns[i][2] and term[:len(pre)] == pre: pos[-1].append(["prn",prns[i][:2]])
 
     #nouns
+    for noun in nstems:
+        if inc(term[0],stem[1:3]): pos[-1].append(["n",noun])
 
     #adjectives
 
@@ -118,6 +135,15 @@ conflict = []
 personal = ["o","s","t","mus","tis","nt","or","ris","tur","mur","mini","ntur"]
 perfects = ["i","isti","it","imus","istis","erunt"]
 persons = ["1S","2S","3S","1P","2P","3P"]
+sufs = ["placeholder",
+["a","ae","ae","am","a","ae","arum","is","as","is"],
+["","i","o","um","o","i","orum","is","os","is"],
+["","is","i","em","e","es","um","ibus","es","ibus"],
+["","us","ui","um","u","us","uum","ibus","us","ibus"],
+["","ei","ei","em","e","es","erum","ebus","es","ebus"]]
+case = ["N","G","D","Ac","Ab"]
+number = ["S","P"]
+vowels = ["a","e","i","o","u"]
 for pts in pos: #possible terms
     fins = [] #finalists
     term = pts.pop(0)
@@ -153,8 +179,17 @@ for pts in pos: #possible terms
                                 else: pers[i] = "e" + pers[i]
 
                         forms.append([root+pers[i],"%s-%s-I-%s"%(persons[i%6],tense,voice)])
-            else:
-                print stem
+                c = stem[0][0]
+                if c == "1": end = "are"
+                elif c == "3": end = "ere"
+                else: end = "re"
+                root = stem[1]
+                if stem[0][2] == "i": root = root[:-1] #any error at this line probably occurs because nounList is empty
+                forms.append([root+end,"P-Inf-A"])
+                if c == "3": end = end[:-3]+"i"
+                else: stem = end[:-1]+"i"
+                forms.append([root+end,"P-Inf-P"])
+            elif p[1] == "pf":
                 for tense in ["Pf","Ppf","Fp"]:
                     root = stem[2]
                     pers = personal[:6]
@@ -166,13 +201,45 @@ for pts in pos: #possible terms
                         if tense != "Pf": pers[i] = "er" + pers[i]
                         forms.append([root+pers[i],"%s-%s-I-A" % (persons[i%6],tense)])
                         if stem[2][-1] == "v": forms.append([root[:-1]+pers[i][1:],"%s-%s-I-A" % (persons[i%6],tense),"sync"]) #syncopation
-
+                forms.append([stem[2]+"isse","Pf-Inf-A"])
+            elif p[1] == "ppl":
+                print "parans parandus"
+            elif p[1] == "ppp":
+                print "paratus paraturus"
             for form in forms:
-                if len(form) != 3: print form
+                if form[0] == term: fins.append([stem,form[1:]])
         elif p[0] == "prn":
-            stem = p[1]
+            stem = p[1][0]
+            if stem == "hic":
+                print "hic haec hoc"
+            elif stem == "qui":
+                print "qui quae quod"
+            elif stem == "is":
+                print "is ea id"
         elif p[0] == "n":
-            print "hashtag text was needed so I'm printing a script"
+            stem = p[1]
+            ends = sufs[stem[0]][:]
+            if stem[3] == "N": [ends[5],ends[8]] = ["a","a"]
+            istem = False
+            if stem[0] == 3:
+                if (stem[1][-1] in ["s","x"] and not (stem[2][-1] in vowels or stem[2][-2] in vowels)): istem = True
+                elif len(stem[1]) > len(stem[2]): istem = True
+                elif stem[3] == "N" and (nom[-2:] in ["al","ar"] or nom[-1] == ["e"]): istem = True
+            for i in range(10):
+                end = ends[i]
+                if istem and i in [4,5,6,8]:
+                    if stem[3] == "N":
+                        if i == 4: end = "i"
+                        elif i!= 6: end = "i" + end
+                    if i == 6: end = "i" + end
+                word = stem[2] + end
+                if i == 0 or [i,stem[3]] == [3,"N"]: word = stem[1]
+                forms.append([word,"%s-%s-%s" % (stem[3],case[i%5],number[i/5])])
+            for form in forms:
+                if form[0] == term: fins.append([stem,form[1]])
         elif p[0] == "adj":
             print "same thing here"
-    if len(fins) != 1: fins.insert(0,"conf") #conflict
+    if len(fins) > 1: fins.insert(0,"conf") #conflict
+    elif len(fins) == 0: fins = ["unknown"]
+    final_list.append(fins)
+#final_list goes to format.py
